3b433edea23888b6788a9e2fc071a0f8866ee0a6
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2020-12-24 12:04:02 -0500

    added backdrop

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,8 +5,15 @@ const addMovieModal = document.getElementById("add-modal");
 const startAddMovieButton = document.querySelector('header button');
 // const startAddMovieButton = document.querySelector('header').lastElementChild;
 
+const backdrop = document.getElementById('backdrop');
+
+const toggleBackdrop = () => {
+    backdrop.classList.toggle('visible');
+};
+
 const toggleMovieModal = () => {
     addMovieModal.classList.toggle('visible');
+    toggleBackdrop();
 };
 
 startAddMovieButton.addEventListener("click", toggleMovieModal);
