d24baa3484db33d7e56c0f26846fa4970b9733ba
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2020-12-24 12:16:50 -0500

    adding multiple ways to toggle backdrop

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -7,6 +7,8 @@ const startAddMovieButton = document.querySelector('header button');
 
 const backdrop = document.getElementById('backdrop');
 
+const cancelAddMovieButton = addMovieModal.querySelector('.btn--passive');
+
 const toggleBackdrop = () => {
     backdrop.classList.toggle('visible');
 };
@@ -16,4 +18,14 @@ const toggleMovieModal = () => {
     toggleBackdrop();
 };
 
+const cancelAddMovie = () => {
+    toggleMovieModal();
+}
+
+const backdropClickHandler = () => {
+    toggleMovieModal();
+}
+
 startAddMovieButton.addEventListener("click", toggleMovieModal);
+backdrop.addEventListener('click', backdropClickHandler);
+cancelAddMovieButton.addEventListener('click', cancelAddMovie);
